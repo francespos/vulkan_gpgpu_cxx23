e9b054049062f30af539b0710af3a4ee4e56463a
francespos <f.esposito.email@gmail.com>
2026-05-04 05:32:04 +0200

    code refactoring

--- src/cxx23_features.cpp
+++ src/cxx23_features.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <expected>
 #include <mdspan>
 #include <print>
@@ -5,7 +6,9 @@
 #include <string_view>
 #include <utility>
 
-std::expected<int, std::string> safe_division(int x, int y) {
+namespace cxx23 {
+
+auto divide(int x, int y) -> std::expected<int, const char*> {
     if (y == 0) {
         return std::unexpected("division for zero");
     }
@@ -13,67 +16,59 @@ std::expected<int, std::string> safe_division(int x, int y) {
     return x / y;
 }
 
-void print_expected_val(std::expected<int, std::string> val) {
-    if (val) {
-        std::println("Result = {}", *val);
-    } else {
-        std::println("Error: {}", val.error());
-    }
-}
-
-void print_matrix(std::mdspan<int, std::dextents<std::size_t, 2>> matrix) {
-    for (size_t i = 0; i < matrix.extent(0); ++i) {
-        for (size_t j = 0; j < matrix.extent(1); ++j) {
-            std::print("{}    ", matrix[i, j]);
-        }
-        std::print("\n");
-    }
-}
-
-class Person {
+class Player {
 public:
-    explicit Person(std::string_view name, std::string_view surname)
-        : name_(name), surname_(surname) {}
+    explicit Player(std::string_view name, unsigned short age) : name_(name), age_(age) {}
 
-    template <typename Self>
-    auto &&name(this Self &&self) {
-        return std::forward<Self>(self).name_;
+    [[nodiscard]] const char* name() const {
+        return name_.c_str();
+    }
+
+    [[nodiscard]] unsigned short age() const {
+        return age_;
     }
 
     template <typename Self>
-    auto &&surname(this Self &&self) {
-        return std::forward<Self>(self).surname_;
+    auto&& score(this Self&& self) {
+        return std::forward<Self>(self).score_;
     }
 
 private:
     std::string name_;
-    std::string surname_;
+    unsigned short age_;
+    int score_ = 0;
 };
 
+} // namespace cxx23
+
 int main() {
+    auto print = [](std::expected<int, const char*> expected) {
+        if (expected) {
+            std::println("{}", *expected);
+        } else {
+            std::println(stderr, "Error: {}.", expected.error());
+        }
+    };
+
     std::println("Computing 3 divided by 1 ...");
-    auto res1 = safe_division(3, 1);
-    print_expected_val(res1);
-    std::print("\n");
+    auto expected1 = cxx23::divide(3, 1);
+    print(expected1);
+    std::println();
 
-    std::println("Computing 3 divided by 0 ... ");
-    auto res2 = safe_division(3, 0);
-    print_expected_val(res2);
-    std::print("\n");
+    std::println("Computing 3 divided by 0 ...");
+    auto expected2 = cxx23::divide(3, 0);
+    print(expected2);
+    std::println();
 
-    // matrix = [1, 2, 3; 4, 5, 6]
-    int matrix_data[] = {1, 2, 3, 4, 5, 6};
-    auto matrix = std::mdspan(matrix_data, 2, 3);
-    std::println("matrix(1, 1) = {}", matrix[1, 1]);
-    std::print("\n");
+    std::array<int, 6> arr{1, 2, 3, 4, 5, 6};
+    auto matrix = std::mdspan(arr.data(), 2, 3);
+    std::println("matrix[1, 1] = {}", matrix[1, 1]);
+    std::println();
 
-    std::println("Printing matrix entries:");
-    print_matrix(matrix);
-    std::print("\n");
+    cxx23::Player player1("Mario", 52);
+    player1.score() = 40;
+    std::println("player1's score is {}", player1.score());
 
-    Person person1("Mario", "Rossi");
-    std::println("person1 is {} {}", person1.name(), person1.surname());
-
-    const Person person2("Luca", "Bianchi");
-    std::println("person2 is {} {}", person2.name(), person2.surname());
+    const cxx23::Player player2("Luca", 17);
+    std::println("player2's score is {}", player2.score());
 }
